5458ca953b8566032f9d5b0c21e742028b910eb6
svgiridharreddy <giridhar.reddy@goshopmatic.com>
2023-04-05 07:45:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import {ToastContainer} from 'react-toastify';
+import http from "./services/httpService";
+import config from "./config.json";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    const {data: posts} = await http.get(config.apiEndpoint)
+    this.setState({ posts })
+  }
+
+  handleAdd = async () => {
+    const obj = {title: "a",body: "b"}
+    const {data: post} = await http.post(config.apiEndpoint,obj)
+    console.log("data",post);
+    const posts = [post, ...this.state.posts]
+    this.setState({posts})
+    
+  };
+
+  handleUpdate = async post => {
+    post.title = "Updated"
+    await http.put(config.apiEndpoint + "/" + post.id, post)
+    const posts = [...this.state.posts]
+    const index = posts.indexOf(post)
+    posts[index] = {...post}
+    this.setState({posts})
+  };
+
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({posts});
+    try {
+      await http.delete("asd" + config.apiEndpoint + "/" + post.id)
+    } catch (error) {
+      if (error.response && error.response.status === 404)
+        alert("Post is alreday deleted!");
+      this.setState({posts: originalPosts});
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
